05700153e4f48c8bceeaae2f4cf26f4ea50539ea
0xzrf <krishh16@yahoo.com>
2025-03-05 20:10:43 +0530

    changed the parse_config function to an associated function to the Config struct

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -1,29 +1,34 @@
 use std::env;
 use std::fs;
 
-
 fn main() {
-    let args: Vec<String> = env::args().collect();    
+    let args: Vec<String> = env::args().collect();
 
-    let (file_path, _query) = parse_config(&args);
+    let config = Config::new(&args);
 
-    let file = fs::read_to_string(file_path);
+    let file = fs::read_to_string(&config.file_path);
 
     let content = match file {
         Ok(file) => file,
         Err(_e) => {
             panic!("No such file");
-        } 
+        }
     };
-    
+
     println!("Content of the file: \n{}", content);
-
 }
 
-
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let file_path = &args[1];
-    let query = &args[2];
-
-    (file_path, query)
+struct Config {
+    file_path: String,
+    query: String,
 }
+
+impl Config {
+    pub fn new(args: &[String]) -> Config {
+        let file_path = args[1].clone();
+        let query = args[2].clone();
+
+        Config { file_path, query }
+    }
+}
+
