9af76c192af81aba98c35c9632b48f048e204651
0xzrf <krishh16@yahoo.com>
2025-03-22 22:16:20 +0530

    updated shell code

---
+++ shell/src/main.rs
@@ -0,0 +1,20 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+fn main() {
+    
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+
+        println!("{}: command not found", input.trim());
+
+    }
+}
