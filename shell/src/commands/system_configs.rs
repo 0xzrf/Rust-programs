f92bee51d48b36e5dc4852fe1564a42145f0d715
0xzrf <krishh16@yahoo.com>
2025-04-14 17:46:15 +0530

    added new code

--- shell/src/commands/system_configs.rs
+++ shell/src/commands/system_configs.rs
@@ -5,16 +5,54 @@ pub struct SystemConfig;
 
 impl SystemConfig {
     pub fn execute_cmd(cmd: &str) -> Result<(), ()>  {
-        let mut v = cmd.split(" ");
-
-        let mut child = match Command::new(v.next().unwrap())
-        .args(v)
-        .spawn() {
-            Ok(val) => val,
-            Err(_) => {
-                return Err(());
+        // Split the command into executable and args while preserving quotes
+        let mut parts = Vec::new();
+        let mut current = String::new();
+        let mut in_quotes = None;
+        
+        for c in cmd.chars() {
+            match c {
+                '"' | '\'' => {
+                    if in_quotes == Some(c) {
+                        in_quotes = None;
+                    } else if in_quotes.is_none() {
+                        in_quotes = Some(c);
+                    } else {
+                        current.push(c);
+                    }
+                }
+                ' ' => {
+                    if in_quotes.is_none() {
+                        if !current.is_empty() {
+                            parts.push(current);
+                            current = String::new();
+                        }
+                    } else {
+                        current.push(c);
+                    }
+                }
+                _ => current.push(c),
             }
-        };
+        }
+        if !current.is_empty() {
+            parts.push(current);
+        }
+
+        if parts.is_empty() {
+            return Err(());
+        }
+
+        let executable = &parts[0];
+        let args = &parts[1..];
+
+        let mut child = match Command::new(executable)
+            .args(args)
+            .spawn() {
+                Ok(val) => val,
+                Err(_) => {
+                    return Err(());
+                }
+            };
 
         match child.wait() {
             Ok(_) => return Ok(()),
