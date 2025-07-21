b876f36e6dfa72ecd8c5efc662d4e99dcf4af063
0xzrf <krishh16@yahoo.com>
2025-07-21 18:41:54 +0530

    completed hello world get request backend

--- backend-server/hello-world/src/main.rs
+++ backend-server/hello-world/src/main.rs
@@ -4,6 +4,10 @@ pub mod routes;
 
 pub use routes::root::hello_world;
 
-fn main() {
-    let router = Router::<&str>::new().route("/", get(hello_world));
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(hello_world));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, router).await.unwrap();
 }
