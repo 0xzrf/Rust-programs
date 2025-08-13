24856dbac10b65a31773b142bb311cb848eb54fc
0xzrf <krishh16@yahoo.com>
2025-08-13 21:30:42 +0530

    added the new code

--- multi-threaded-web-server/src/main.rs
+++ multi-threaded-web-server/src/main.rs
@@ -1,4 +1,6 @@
-use std::net::TcpListener;
+use std::fs;
+use std::io::{BufReader, prelude::*};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +8,23 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let stream_buffer = BufReader::new(&stream);
+    let http_buffer = stream_buffer.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if http_buffer == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let length = content.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
