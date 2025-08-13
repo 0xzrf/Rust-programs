819fe8f3a6921e9bc3fea44d0fa3683dfdf515a3
0xzrf <krishh16@yahoo.com>
2025-08-13 23:23:52 +0530

    added the demonstration for the fact that the current http server is a single threaded one

--- multi-threaded-web-server/src/main.rs
+++ multi-threaded-web-server/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::io::{BufReader, prelude::*};
 use std::net::{TcpListener, TcpStream};
+use std::{fs, thread, time::Duration};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,10 +16,13 @@ fn handle_connection(mut stream: TcpStream) {
     let stream_buffer = BufReader::new(&stream);
     let http_buffer = stream_buffer.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if http_buffer == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &http_buffer[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let content = fs::read_to_string(filename).unwrap();
