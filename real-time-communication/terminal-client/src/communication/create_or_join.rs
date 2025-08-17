8ddef3dd006d483f1cfc43a60e014d19bfdedfec
0xzrf <krishh16@yahoo.com>
2025-08-17 12:15:38 +0530

    tests receiving and sending happening

--- real-time-communication/terminal-client/src/communication/create_or_join.rs
+++ real-time-communication/terminal-client/src/communication/create_or_join.rs
@@ -2,8 +2,13 @@ use crate::{
     errors::{CreateErrors, JoinErrors, OnboardErrors},
     user_onboard::print_help,
 };
-use std::io::{self, Write};
+use std::{
+    io::{self, Read, Write},
+    net::TcpStream,
+    time::Duration,
+};
 
+use tokio::time::sleep;
 pub struct Communication {
     pub user_name: String,
     pub joined_room: Option<String>,
@@ -48,10 +53,54 @@ impl Communication {
     /// This function is used to join the room in the server
     /// It will simply send some TCP requests to it and then start messaging it
     async fn create_room(&self) -> Result<(), CreateErrors> {
+        self.connect_server()
+            .await
+            .expect("Couldn't connect to server");
         Ok(())
     }
 
     fn join_room(&self) -> Result<(), JoinErrors> {
         todo!()
+    }
+
+    async fn connect_server(&self) -> Result<(), OnboardErrors> {
+        // Connect to the first nc listener (terminal 1)
+        if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8080") {
+            let mut str_clone = stream.try_clone().unwrap();
+            let handle = tokio::task::spawn(async move {
+                loop {
+                    let mut buffer = [0; 512];
+
+                    while let Ok(n) = stream.read(&mut buffer) {
+                        if n == 0 {
+                            println!("Closing connection");
+                            break; // connection closed
+                        }
+                        println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+                        stream.flush().unwrap();
+                    }
+                }
+            });
+
+            println!("Joining and sending");
+            let join_bfr = b"{\"type\":\"JoinRoom\",\"room\":\"lobby\"}\n";
+            let msg_bfr = b"{\"type\":\"Message\",\"room\":\"lobby\",\"from\":\"server\",\"text\":\"hello terminal 1\"}\n";
+
+            str_clone.write_all(join_bfr).expect("Couldn't send buffer");
+            str_clone.flush().unwrap();
+
+            for _ in 0..100 {
+                println!("Sending buffer");
+                str_clone.write_all(msg_bfr).expect("Couldn't send buffer");
+                str_clone.flush().unwrap();
+                sleep(Duration::from_secs(5)).await;
+            }
+
+            handle.await.unwrap();
+        } else {
+            eprintln!("Could not connect to listener 1");
+        }
+
+        Ok(())
     }
 }
--- real-time-communication/terminal-client/src/errors/onboard_errors.rs
+++ real-time-communication/terminal-client/src/errors/onboard_errors.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 pub enum OnboardErrors {
-    InvalidCommand(&'static str),
     CreateErrors(&'static str),
     JoinErrors(&'static str),
+    ServerError(&'static str),
 }
 
 #[derive(Debug)]
@@ -15,10 +15,22 @@ pub enum JoinErrors {
     RoomNotJoined(&'static str),
 }
 
+#[derive(Debug)]
+pub enum ServerConnectionError {
+    CouldntConnectServer(&'static str),
+}
+
 impl From<CreateErrors> for OnboardErrors {
     fn from(value: CreateErrors) -> Self {
         match value {
             CreateErrors::RoomNotCreated(str) => Self::CreateErrors(str),
+        }
+    }
+}
+impl From<ServerConnectionError> for OnboardErrors {
+    fn from(value: ServerConnectionError) -> Self {
+        match value {
+            ServerConnectionError::CouldntConnectServer(str) => Self::CreateErrors(str),
         }
     }
 }
