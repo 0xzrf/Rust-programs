fb5e6387566cfd71cd3ae8e9cd19db11939b80ba
0xzrf <krishh16@yahoo.com>
2025-08-22 13:59:26 +0530

    implemented st_async_io method for higher throuput

--- multi-threaded-web-server/src/main.rs
+++ multi-threaded-web-server/src/main.rs
@@ -2,13 +2,28 @@ use multi_threaded_web_server::STAsyncIO;
 use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = STAsyncIO::new(5).await;
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming().take(2) {
-        let stream = stream.unwrap();
+    loop {
+        let (mut stream, addr) = listener.accept().await.unwrap();
+        println!("New connection from: {addr}");
 
-        pool.execute_st_io();
+        // spawn a task to handle this connection
+        let st_async_io = STAsyncIO;
+
+        st_async_io.execute(stream).await;
     }
+
+    // Code to execute thread pool
+    // let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // let pool = ThreadPool::new(5);
+
+    // for stream in listener.incoming().take(2) {
+    //     let stream = stream.unwrap();
+
+    //     pool.run_thread_pooling(stream);
+    // }
+    // println!("Shutting down the server");
+
     println!("Shutting down the server");
 }
--- multi-threaded-web-server/src/st_async_io/st_io.rs
+++ multi-threaded-web-server/src/st_async_io/st_io.rs
@@ -1,77 +1,12 @@
-use std::sync::Arc;
-
 use crate::async_handle_connection;
-use tokio::{
-    net::TcpStream,
-    sync::{
-        Mutex,
-        mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel},
-    },
-    task::{self, JoinHandle},
-};
-pub struct Worker {
-    pub task: JoinHandle<()>,
-    pub id: usize,
-}
+use tokio::net::TcpStream;
 
-pub struct STAsyncIO {
-    workers: Vec<Worker>,
-    sender: UnboundedSender<Job>,
-}
+pub struct STAsyncIO;
 
 impl STAsyncIO {
-    pub async fn new(size: usize) -> Self {
-        assert!(size > 0);
-
-        let mut workers = Vec::with_capacity(size);
-
-        let (tx, mut rx) = unbounded_channel();
-
-        let rx_arc = Arc::new(Mutex::new(rx));
-
-        for i in 0..size {
-            let worker = Worker::new(i, Arc::clone(&rx_arc)).await;
-
-            workers.push(worker);
-        }
-
-        Self {
-            sender: tx,
-            workers,
-        }
-    }
-
-    pub async fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-        self.sender.send(job).unwrap();
-    }
-    pub async fn run_st_async_io<F>(&self, stream: TcpStream)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        self.execute(|| {
-            async_handle_connection(stream);
+    pub async fn execute(&self, stream: TcpStream) {
+        tokio::spawn(async move {
+            async_handle_connection(stream).await;
         });
-    }
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-impl Worker {
-    pub async fn new(id: usize, recv: Arc<Mutex<UnboundedReceiver<Job>>>) -> Self {
-        let task = task::spawn(async move {
-            loop {
-                let mut job_lock = recv.lock().await;
-
-                if let Some(job) = job_lock.recv().await {
-                    println!("Worker {id} gor the job, executing...");
-                    job();
-                }
-            }
-        });
-
-        Worker { id, task }
     }
 }
